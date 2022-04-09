329a207f72c79aa488f96336d5c04e5323210451
Temur <https://github.com/UNkn0wn1111>
2022-04-09 15:49:28 +0500

    Initial commit

---
+++ src/main/java/uz/project/emed/EmedApplication.java
@@ -0,0 +1,13 @@
+package uz.project.emed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/project/emed/EmedApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.project.emed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
